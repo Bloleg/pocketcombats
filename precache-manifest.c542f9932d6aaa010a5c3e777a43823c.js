59901400cd8245c52dc9226956f51073f07ba9bf
Bloleg <53895536+Bloleg@users.noreply.github.com>
2020-01-16 14:21:49 +0300

    Add files via upload

---
+++ precache-manifest.c542f9932d6aaa010a5c3e777a43823c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbce1c86b81158bba922796cbcea893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f540b735f7cfd09e7d7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6c1411277ba3b55b60cd",
+    "url": "/static/js/2.276d6a1b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.276d6a1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f540b735f7cfd09e7d7",
+    "url": "/static/js/main.5872344a.chunk.js"
+  },
+  {
+    "revision": "fc70b38f117949b1a81f",
+    "url": "/static/js/runtime-main.51662271.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
